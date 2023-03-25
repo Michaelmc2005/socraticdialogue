6caf483478d1b452ac6241f905e082247d21179b
Michaelmc2005 <michaelmc2005@gmail.com>
2023-03-25 21:13:17 +0000

    rescaling for mobile

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -2,29 +2,31 @@
 	import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
 	export let type: ChatCompletionRequestMessageRoleEnum
 	export let message: string
-</script>
-
-<div class="chat {type === 'user' ? 'chat-end' : 'chat-start'} justify-end">
+  </script>
+  
+  <div class="chat {type === 'user' ? 'chat-end' : 'chat-start'} justify-flex" style="justify-content: {type === 'user' ? 'flex-end' : 'flex-start'};">
 	<div class="chat-image avatar">
-		<div class="w-10 rounded-full">
-			<img
-				src="https://ui-avatars.com/api/?name={type === 'user' ? 'Me' : 'S'}"
-				alt="{type} avatar"
-			/>
-		</div>
+	  <div class="w-10 rounded-full">
+		<img
+		  src="https://ui-avatars.com/api/?name={type === 'user' ? 'Me' : 'S'}"
+		  alt="{type} avatar"
+		/>
+	  </div>
 	</div>
 	<div class="chat-header">
-		{type === 'user' ? 'Me' : 'Socratique'}
+	  {type === 'user' ? 'Me' : 'Socratique'}
 	</div>
 	<div class="chat-bubble {type === 'user' ? 'chat-bubble-primary' : 'chat-bubble-secondary'}">
-		{message}
+	  {message}
 	</div>
-</div>
-<style>
+  </div>
+  
+  <style>
 	/* Customize the chat bubble colors */
 	.chat-bubble-primary {
 	  background-color: #CECECF ;
 	  color: #333;
+	  
 	}
   
 	.chat-bubble-primary-light {
@@ -51,3 +53,4 @@
 	  color: #333;
 	}
   </style>
+  
